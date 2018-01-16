0086c8b341416d21d9ea4a3b3b8d825373fe9feb
TadB <tadeusz.brzytwa@gmail.com>
2018-01-16 17:17:59 +0100

    budowa fabryki
    poczatek drogi

---
+++ factory.cpp
@@ -0,0 +1,11 @@
+#include "factory.h"
+
+//konstruktory
+factory::factory(){
+    cout<<"nie ma pustych fabryk!";
+    exit(0);
+}
+
+factory::factory(int tableNo, int lockerNo, int legNo, int desktopNo, int doorNo, int casingNo){
+    
+}
--- factory.h
+++ factory.h
@@ -1,7 +1,26 @@
 //to bedzie moja fabryka - czyli klasa graf, ktora bedzie zestawiac polaczenia pomiedzy stanowiskami
 #ifndef FACTORY_H
 #define FACTORY_H
+#include"product.h"
+#include"element.h"
 
+class factory{
+private:
+    // wektory jako pudelka na stanowiska montujace i wytwarzajace poszczegolne przedmioty
+    vector<product> table;
+    vector<product> locker; //szafki xd
 
+    vector<element> leg;
+    vector<element> desktop;
+    vector<element> door;
+    vector<element> casing; //obudowa
+
+    //magazyn wyprodukowanych elementow
+    int storehouseTable, storehouseLocker;
+public:
+    factory();
+    factory(int, int, int, int, int, int);
+
+};
 
 #endif
