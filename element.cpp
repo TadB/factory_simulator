32032c9b7624b3f6f102558bc5393a5119449ca8
TadB <tadeusz.brzytwa@gmail.com>
2018-01-16 16:08:16 +0100

    obiekty grafu

---
+++ element.cpp
@@ -0,0 +1,5 @@
+#include "element.h"
+
+void element::addSlotAdress(product *item){
+    path.push_back(item);
+}
--- element.h
+++ element.h
@@ -8,7 +8,7 @@ protected:
     // string name;
     vector<product*>path; //wektor wskaznikow na produkty, ktore beda wytwarzane z tych elementow
 public:
-    void timeStep();//krok czasowy po ktorym wysylany jest przedmiot do nastepnego stanowiska
+    //  void timeStep();//krok czasowy po ktorym wysylany jest przedmiot do nastepnego stanowiska
     void getDirection(); //decyduje gdzie przekazac nastepny element(do ktorego stanowiska) - decyduje dlugosc kolejki przy stanowisku
     void addSlotAdress(product*);//dodaje kolejny wskaznik do wektora path, dodaje ten ktory dostalo w argumencie
 
