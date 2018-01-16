3b76762ba41cbcdcd52bbf38d5e39716eed2aba6
TadB <tadeusz.brzytwa@gmail.com>
2018-01-16 16:50:16 +0100

    poprawiona logika

--- element.cpp
+++ element.cpp
@@ -1,5 +1,21 @@
 #include "element.h"
 
-void element::addSlotAdress(product *item){
+void element::addSlotAdress(queue<int> *item){
     path.push_back(item);
+}
+
+queue<int>* element::getDirection(){
+    // queue<int> *minAdress;
+    unsigned int minValue;
+    vector<queue<int>*>::iterator it=path.begin();
+    queue<int> *minAdress = *it;
+    minValue=(*it)->size();
+    it++; //zeby nie sprawdzac potem tego co juz przypisalismy do minimum czy jest minimum
+    for(; it<path.end(); it++){
+        if(((*it)->size())<minValue){
+            minAdress=*it;
+            minValue=(*it)->size();
+        }
+    }//petla poszukuje najmniejszej(najkrotszej) kolejki
+    return(minAdress);
 }
--- element.h
+++ element.h
@@ -6,11 +6,11 @@
 class element{
 protected:
     // string name;
-    vector<product*>path; //wektor wskaznikow na produkty, ktore beda wytwarzane z tych elementow
+    vector<queue<int>*>path; //wektor wskaznikow na produkty, ktore beda wytwarzane z tych elementow
 public:
     //  void timeStep();//krok czasowy po ktorym wysylany jest przedmiot do nastepnego stanowiska
-    void getDirection(); //decyduje gdzie przekazac nastepny element(do ktorego stanowiska) - decyduje dlugosc kolejki przy stanowisku
-    void addSlotAdress(product*);//dodaje kolejny wskaznik do wektora path, dodaje ten ktory dostalo w argumencie
+    queue<int>* getDirection(); //decyduje gdzie przekazac nastepny element(do ktorego stanowiska) - decyduje dlugosc kolejki przy stanowisku
+    void addSlotAdress(queue<int>*);//dodaje kolejny wskaznik do wektora path, dodaje ten ktory dostalo w argumencie
 
 };
 
